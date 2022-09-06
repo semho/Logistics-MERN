df46e10716886dd8b95b9ba31483235d5ec8cc38
unknown <semho@yandex.ru>
2022-09-06 08:19:02 +0300

    shared the logic of authorization registration

--- client/src/pages/RegPage/RegPage.tsx
+++ client/src/pages/RegPage/RegPage.tsx
@@ -1,16 +1,19 @@
 import React, { useCallback, useEffect, useState } from "react";
-import { ButtonStyled } from "../../components/ButtonStyled/ButtonStyled";
+import { Link } from "react-router-dom";
+import { toast } from "react-toastify";
+import { ButtonStyled } from "../../components/ButtonStyled";
 import { InputStyled } from "../../components/InputStyled";
 import { LabelStyled } from "../../components/LabelStyled";
-import { useMessage } from "../../hooks/useMessage";
-
-import { useAppDispatch, useAppSelector } from "../../redux/store";
-import { dataUser, IStatusUser, login } from "../../redux/features/authSlice";
-import { toast } from "react-toastify";
 import { Loader } from "../../components/Loader";
-import { Link } from "react-router-dom";
+import { useMessage } from "../../hooks/useMessage";
+import {
+  dataUser,
+  IStatusUser,
+  register,
+} from "../../redux/features/authSlice";
+import { useAppDispatch, useAppSelector } from "../../redux/store";
 
-export function AuthPage() {
+export function RegPage() {
   const [errorAuth, setAuthError] = useState("");
   const {
     statusUser: { loading, error },
@@ -34,9 +37,9 @@ export function AuthPage() {
     setForm({ ...form, [event.target.name]: event.target.value });
   };
 
-  const loginHandler = async () => {
+  const registerHandler = async () => {
     const formValue = { ...form };
-    dispatch(login({ formValue, toast }));
+    dispatch(register({ formValue, toast }));
   };
 
   if (loading) {
@@ -47,7 +50,7 @@ export function AuthPage() {
     <div className="h-screen relative">
       <div className="absolute top-1/4 left-1/2 transform -translate-x-1/2 -translate-y-1/4 shadow-xl rounded divide-y-2 w-5/6 md:w-1/2 lg:w-1/4">
         <div className="px-5 pb-3">
-          <h1 className="text-[42px] text-center mb-5 ">Вход</h1>
+          <h1 className="text-[42px] text-center mb-5 ">Регистрация</h1>
 
           <div className="form-group mb-6">
             <LabelStyled title="Email" forId="inputEmail" textColor="gray" />
@@ -77,19 +80,19 @@ export function AuthPage() {
           </div>
           <div className="w-full">
             <ButtonStyled
-              title="Авторизоваться"
-              variant="purple"
+              title="Зарегистироваться"
+              variant="sky"
               type="button"
-              onClick={loginHandler}
+              onClick={registerHandler}
               disabled={loading}
               className="w-full"
             />
           </div>
         </div>
         <p className="p-3 text-center">
-          Еще не зарегистированы?{" "}
-          <Link to="/reg" className="purple-color">
-            Зарегистрироваться
+          Уже зарегистированы?{" "}
+          <Link to="/auth" className="purple-color">
+            Авторизоваться
           </Link>
         </p>
       </div>

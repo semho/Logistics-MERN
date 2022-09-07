ef86775bed4c2673479c4992503c0f5df8333f47
unknown <semho@yandex.ru>
2022-09-07 19:47:30 +0300

    addel localStorage in middleware

--- client/src/redux/store.ts
+++ client/src/redux/store.ts
@@ -1,13 +1,31 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
 import AuthReducer from "./features/authSlice";
 import RecordReducer from "./features/recordSlice";
+
+//NOTE: собираем localStorage для помещения данных в Middleware
+const localStorageMiddleware = ({ getState }: any) => {
+  return (next: any) => (action: any) => {
+    const result = next(action);
+    localStorage.setItem("AppLogistics", JSON.stringify(getState()));
+    return result;
+  };
+};
+//тут извлекаем из localStore
+const reHydrateStore = () => {
+  if (localStorage.getItem("AppLogistics") !== null) {
+    return JSON.parse(localStorage.getItem("AppLogistics") || "{}"); // re-hydrate the store
+  }
+};
 
 const store = configureStore({
   reducer: {
     auth: AuthReducer,
     records: RecordReducer,
   },
+  preloadedState: reHydrateStore(),
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(localStorageMiddleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;

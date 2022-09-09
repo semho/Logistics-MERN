2ba17be57fe11b65a576ce3b7ca5f2e2e89e2cc4
unknown <semho@yandex.ru>
2022-09-09 19:57:56 +0300

    added api for settingsDestination

--- client/src/redux/api.ts
+++ client/src/redux/api.ts
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { IRecord } from "../models/Record";
+import { ISettingsDestination } from "../models/Settings";
 import { IUser } from "../models/User";
 
 //авторизация/регистрация
@@ -27,5 +28,28 @@ export const deleteRecord = (id: string, token: string) =>
 
 export const updateRecord = (record: IRecord, token: string) =>
   axios.put("/api/records/update", record, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+//запросы на добавление пунктов отправления-назначения
+export const allDestinations = (token: string) =>
+  axios.get("/api/settings/destination/", {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+export const newDestination = (record: {}, token: string) =>
+  axios.post("/api/settings/destination/create", record, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+
+export const deleteDestination = (id: string, token: string) =>
+  axios.delete("/api/settings/destination/delete", {
+    headers: { Authorization: `Bearer ${token}` },
+    data: { id },
+  });
+
+export const updateDestination = (
+  record: ISettingsDestination,
+  token: string
+) =>
+  axios.put("/api/settings/destination/update", record, {
     headers: { Authorization: `Bearer ${token}` },
   });
